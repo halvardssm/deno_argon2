7d7700c0f6af8319e1ce8d54651f3cea521a7013
Halvard Mørstad <halvard@simplyundoable.com>
2024-03-26 11:16:38 +0100

    simplified lib

--- src/lib.rs
+++ src/lib.rs
@@ -39,60 +39,16 @@ impl Default for Algorithm {
 }
 
 #[wasm_bindgen]
-#[derive(Default)]
-#[derive(Debug)]
-pub struct HashOptions {
-    /// Argon2 alorithm to use, can be one of Argon2d, Argon2i, or Argon2id.
-    /// Default is Argon2id.
-    pub algorithm: Option<Algorithm>,
-    /// Memory size, expressed in kilobytes, between 1 and (2^32)-1.
-    ///
-    /// Value is an integer in decimal (1 to 10 digits).
-    #[wasm_bindgen(js_name = memoryCost)]
-    pub memory_cost: Option<u32>,
-    
-    /// Number of iterations, between 1 and (2^32)-1.
-    ///
-    /// Value is an integer in decimal (1 to 10 digits).
-    #[wasm_bindgen(js_name = timeCost)]
-    pub time_cost: Option<u32>,
-    
-    /// Degree of parallelism, between 1 and 255.
-    ///
-    /// Value is an integer in decimal (1 to 3 digits).
-    #[wasm_bindgen(js_name = parallelismCost)]
-    pub parallelism_cost: Option<u32>,
-    
-    /// Size of the output (in bytes).
-    #[wasm_bindgen(js_name = outputLength)]
-    pub output_length: Option<usize>,
-}
-
-#[wasm_bindgen]
-pub fn hash(password: &str, options: Option<HashOptions>) -> String {
-    let opts = options.unwrap_or_default();
-    println!("opts: {:?}", opts);
-    
-    let algorithm = match opts.algorithm.unwrap_or_default() {
+pub fn hash(password: &str, algorithm: Option<Algorithm>) -> String {    
+    let algorithm = match algorithm.unwrap_or_default() {
         Algorithm::Argon2d => argon2::Algorithm::Argon2d,
         Algorithm::Argon2i => argon2::Algorithm::Argon2i,
         Algorithm::Argon2id => argon2::Algorithm::Argon2id,
     };
-    println!("algorithm: {:?}", algorithm);
     
     let default_params = argon2::Params::default();
-    println!("default_params: {:?}", default_params);
 
-    let params = argon2::Params::new(
-        opts.memory_cost.unwrap_or(default_params.m_cost()),
-        opts.time_cost.unwrap_or(default_params.t_cost()),
-        opts.parallelism_cost.unwrap_or(default_params.p_cost()),
-        Some(opts.output_length.unwrap_or(default_params.output_len().unwrap_or_default()))
-    ).expect("bad argon2 parameters");
-
-    println!("params: {:?}", params);
-
-    let argon2 = Argon2::new(algorithm, Version::V0x13, params);
+    let argon2 = Argon2::new(algorithm, Version::V0x13, default_params);
     let salt = SaltString::generate(&mut OsRng);
     let password_bytes = password.as_bytes();
     argon2
